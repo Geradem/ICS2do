2340bca6fedb3ed3b7d26f59eb4ba4c6520b9939
Geradem <Geradem@users.noreply.github.com>
2025-06-03 19:04:57 +0000

    deploy: b274a721727214546bd0ef754d7af33e7063a46a

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+export const contadorCaracteres = (elemento, idContenedor) => {
+   let contenedorElement = document.getElementById(idContenedor);
+   if (!contenedorElement) {
+       console.warn(`No se encontró un elemento con el ID: ${idContenedor}`);
+       return;
+   }
+   let tamanioTexto = elemento.value.length;
+   console.log('tamanioTexto :>>', tamanioTexto);
+   contenedorElement.innerHTML = `<span>${tamanioTexto} Caracteres </span>`;
+};
+
+// Hacer que la función sea accesible globalmente
+window.contadorCaracteres = contadorCaracteres;
